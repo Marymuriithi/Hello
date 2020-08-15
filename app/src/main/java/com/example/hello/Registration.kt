21890ed30fa4df3122f7c594d9c60ba087f44e1e
MaryMuriithi <mariemuriithi771@gmail.com>
2020-08-15 08:49:50 +0300

    registration

--- app/src/main/java/com/example/hello/Registration.kt
+++ app/src/main/java/com/example/hello/Registration.kt
@@ -1,11 +1,59 @@
-package com.example.hello
 
-import androidx.appcompat.app.AppCompatActivity
+}package ke.co.hello
+
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_registration.*
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class Registration : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class RegistrationActivity : AppCompatActivity() {
+    override fun
+            onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+        -177 +2342class RegistrationActivity: AppCompatActivity() {
+            val phoneNumber = etPhoneNumber.text.toString()
+            val password = etPassword.text.toString()
+            val passwordConfirmation = etConfirmPassword.text.toString()
+
+            var requestBody = MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("first_name", firstName)
+                    .addFormDataPart("last_name", lastName)
+                    .addFormDataPart("email", email)
+                    .addFormDataPart("phone_number", phoneNumber)
+                    .addFormDataPart("password", password)
+                    .build()
+
+        }
+    }
+
+
+    fun registerUser(requestBody: RequestBody) {
+        var apiClient = ApiClient.buildService(ApiInterface::class.java)
+        var registrationCall = apiClient.registerStudent(requestBody)
+        registrationCall.enqueue(object : Callback<RegistrationResponse> {
+             fun onFailure(call: Call<RegistrationResponse>, t: Throwable) {
+                Toast.makeText(baseContext, t.message, Toast.LENGTH_LONG).show()
+            }
+
+             fun onResponse(
+                    call: Call<RegistrationResponse>,
+                    response: Response<RegistrationResponse>
+            ) {
+                if (response.isSuccessful) {
+                    Toast.makeText(baseContext, response.body()?.message, Toast.LENGTH_LONG).show()
+                    startActivity(Intent(baseContext, MainActivity::class.java))
+                } else {
+                    Toast.makeText(baseContext, response.errorBody().toString(), Toast.LENGTH_LONG)
+                            .show()
+                }
+            }
+        })
     }
 }
---
+++ app/src/main/res/layout/build/gradle.kt
@@ -0,0 +1,4 @@
+package layout.build
+
+object gradle {
+}
